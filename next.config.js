e1bbc51845783e36c59c1bf6f3d724511dafad20
pixelberry-jin <viva-jinyi@kakao.com>
2023-11-08 20:06:33 +0900

    build: next config modified

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://accounts.google.com' },
+        ],
+      },
+    ];
+  },
+};
