782b73346c6ce21aa7f6ccc4a355099ca3dec3cb
pixelberry-jin <viva-jinyi@kakao.com>
2023-11-08 09:48:55 +0900

    feature: lunar holiday added

--- src/utils/getSolarDate.ts
+++ src/utils/getSolarDate.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 import "moment-lunar";
 
-export function convertLunarToSolar(lunarDate: string) {
-  return moment(lunarDate, "YYYY-MM-DD").solar();
+export function convertLunarToSolar(lunarDate: string): string {
+  return moment(lunarDate, "YYYY-MM-DD").solar().format("YYYY-MM-DD");
 }
